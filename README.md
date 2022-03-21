60b96aa14f7e5227ac418fbfc8a12ec2a097fb5a
Jonah Gourlay <jonah.gourlay44@gmail.com>
2022-03-20 20:28:13 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ZeroShotRT
-An advanced educational platform for analyzing zero-shot reinforcement learning algorithms in realtime.
+An advanced educational platform for analyzing reinforcement learning algorithms in realtime.
+
+# Required Materials
+- Jetson development board running L4T version 32.6.1 or later
+
+# Installing
+First, clone this repository onto your Jetson development board with 
+
+        git clone --recurse-submodules https://github.com/jonah-gourlay44/ZeroShotRT
+
+From the ZeroShotRT directory, build the Docker image with
+
+        nvidia-docker build -t zeroshotrt .
+
+Perform the post installation steps with 
+
+        sudo scripts/post_inst
+
+Finally, reboot your system to complete the installation
+
+# Usage
+Run a new container from the zeroshotrt Docker image with
+
+        ./scripts/docker_run {container_name}
+
+The container can be restarted after exiting with
+
+        docker start -i {container_name}
+
---
+++ workspace/core/include/core/device.hpp
@@ -0,0 +1,114 @@
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+#include <i2c/i2c.h>
+
+class Device
+{
+public:
+    Device()
+    {}
+
+    ~Device() {}
+
+    bool connect() = 0;
+
+    void checkConnected()
+    {
+        if (!initialized_) {
+            throw std::runtime_error("The device has not been initialized.");
+        }
+    }
+
+protected:
+    bool initialized_;
+};
+
+class ConnectionI2C : public Device
+{
+public:
+    ConnectionI2C(int address, int scl_pin, int sda_pin)
+        : Device()
+        , address_(address)
+        , scl_pin_(scl_pin)
+        , sda_pin_(sda_pin)
+    {}
+
+    ~ConnectionI2C() {}
+
+    bool connect() override
+    {
+        std::string sda_bus = sda_map.at(sda_pin);
+        std::string scl_bus = scl_map.at(scl_pin);
+
+        if (sda_bus != scl_bus) {
+            printf("Pins are from mismatched I2C buses.\n");
+            initialized_ = false;
+            return false;
+        }
+
+        if ((bus_ = i2c_open(sda_bus.c_str())) == -1) {
+            printf("Failed to opend the device [%s].\n", sda_bus.c_str());
+            initilized_ = false;
+            return false;
+        }
+
+        i2c_init_device(&device_);
+        initialized_ = true;
+
+        device_.addr = address_;
+        device_.bus = bus_;
+
+        return initialized_;
+    }
+
+    template <typename T>
+    bool read(int address, T* data, int length)
+    {
+        checkConnected();
+        size_t size = sizeof(T) * length;
+        size_t read_size;
+        if ((read_size = i2c_read(&device_, address, data, size)) == -1) {
+            return false;
+        } else if (read_size != size) {
+            return false;
+        }
+    }
+
+    template <typename T>
+    bool write(int address, T* data, int length)
+    {
+        checkConnected();
+        size_t size = sizeof(T) * length;
+        size_t write_size;
+        
+        if ((write_size = i2c_write(&device_, address, buf, buf_size)) == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+protected:
+    int address_;
+    int bus_;
+    int scl_pin_;
+    int sda_pin_;
+
+    static const std::map<int, std::string> sda_map;
+    static const std::map<int, std::string> scl_map;
+
+    I2CDevice device_;
+};
+
+std::map<int, std::string> ConnectionI2C::sda_map = {
+    {27, "/dev/i2c-0"},
+    {3, "/dev/i2c-1"}
+};
+
+std::map<int, std::string> ConnectionI2C::scl_map = {
+    {28, "/dev/i2c-0"},
+    {5, "/dev/i2c-1"}
+};
+
+#endif
