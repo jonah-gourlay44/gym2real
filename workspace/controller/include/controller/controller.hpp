8fc6ccbe7472c8b5264eb39e6b8640ef677ea3f3
Kevin Huang <kevinh42@student.ubc.ca>
2022-03-21 21:57:13 -0700

    Added basic implementation of controller

---
+++ workspace/controller/include/controller/controller.hpp
@@ -0,0 +1,133 @@
+#pragma once
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <onnxruntime_cxx_api.h>
+#include <onnxruntime_c_api.h>
+#include <stdio.h>
+#include <assert.h>
+#include <controller/transform.hpp>
+
+/** Controllers should be defined with relevant transforms/remaps
+ *  Should be used whenever data is being copied 
+ *  
+ *  Example:
+ * 
+ *  `
+ *  - BalanceController:
+ *      observations: 4 # Number of observations
+ *      actions: 2 # Number of actions
+ *      transforms:
+ *          - transform_1
+ *          - transform_2
+ *  `
+ */
+class BaseController
+{
+public:
+    BaseController(
+        int observations, 
+        int actions) :
+        observations_(observations), 
+        actions_(actions)
+    {
+        input_buffer_ = new float[observations];
+        output_buffer_ = new float[actions];
+    };
+
+    ~BaseController(){
+        delete[] input_buffer_;
+        delete[] output_buffer_;
+    };
+
+    int run()
+    {
+        //Pre-transforms (load data into buffers)
+        for (auto &trans : pre_transforms_)
+        {
+            trans.apply();
+        }
+
+        //Main control step (inference)
+        control_step();
+
+        //Post-transforms (write data to buffers)
+        for (auto &trans : post_transforms_)
+        {
+            trans.apply();
+        }
+        return 0;
+    };
+
+    int addTransforms(std::vector<TransformRule<float, float>> pre_transforms, std::vector<TransformRule<float, float>> post_transforms){
+        pre_transforms_ = pre_transforms;
+        post_transforms_ = post_transforms;
+        return 0;
+    };
+
+    std::pair<float*,float*> getBuffers(){
+        return std::pair<float*,float*>(input_buffer_,output_buffer_);
+    };
+protected:
+    virtual int init(){return 0;};
+    virtual int control_step(){return 0;};
+
+    int observations_;
+    int actions_;
+    std::vector<TransformRule<float, float>> pre_transforms_;
+    std::vector<TransformRule<float, float>> post_transforms_;
+
+    float *input_buffer_ = nullptr;
+    float *output_buffer_ = nullptr;
+};
+
+
+class OnnxController : public BaseController
+{
+public:
+    OnnxController(
+        int observations, 
+        int actions,
+        std::string model_path
+        ) : model_path_(model_path), BaseController(observations,actions)
+    {
+        init();
+    };
+
+    int init()
+    {
+        session_ = Ort::Session{env_, model_path_.c_str(), Ort::SessionOptions{nullptr}};
+        input_shape_ = {observations_};
+        output_shape_ = {actions_};
+
+        auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+
+        input_tensor_ = Ort::Value::CreateTensor<float>(memory_info, input_buffer_, observations_ * sizeof(float), input_shape_.data(), input_shape_.size());
+        output_tensor_ = Ort::Value::CreateTensor<float>(memory_info, output_buffer_, actions_ * sizeof(float), output_shape_.data(), output_shape_.size());
+        return 0;
+    };
+
+protected:
+    int control_step()
+    {
+        const char *input_names[] = {"observations"};
+        const char *output_names[] = {"actions"};
+
+        session_.Run(opt_, input_names, &input_tensor_, 1, output_names, &output_tensor_, 1);
+
+        return 0;
+    }
+
+private:
+    std::string model_path_;
+    Ort::Env env_;
+    Ort::Session session_{nullptr};
+    Ort::RunOptions opt_{nullptr};
+
+    Ort::Value input_tensor_{nullptr};
+    std::array<int64_t, 1> input_shape_;
+
+    Ort::Value output_tensor_{nullptr};
+    std::array<int64_t, 1> output_shape_;
+};
---
+++ workspace/controller/include/controller/transform.hpp
@@ -0,0 +1,138 @@
+#pragma once
+/** Remaps copy data from one buffer to another and remaps/inverts/clips based on configuration.
+ *  Data in the memory stores should always be in the same space for easier troubleshooting.
+ *
+ *  Examples:
+ *
+ *   Interfacing with actuators (assumes actuator commands and feedback are in same space):
+ *   `
+ *   - actuator_1: # Actuator space (depends on actuator firmware) to joint space (URDF)
+ *       actuator_range: [-1, 1] # Values that should be given to motor interface
+ *       joint: joint_1 # Gets limits from URDF
+ *       invert: True
+ *       type: effort # position/velocity/effort
+ *
+ *   - actuator_2: # Actuator space (depends on actuator firmware) to joint space (URDF)
+ *       actuator_range: [-1, 1] # Values that should be given to motor interface
+ *       joint_range: [0, 3.14159] # Overrides limits from URDF
+ *       invert: False
+ *       type: effort # position/velocity/effort
+ *   `
+ *
+ *   Interfacing with sensors:
+ *   `
+ *   - sensor_1: # Sensor space
+ *       sensor_range: [0, 255]
+ *       controller_range: [0, 1]
+ *       invert: False
+ *   `
+ **/
+#include <string>
+#include <vector>
+#include <functional>
+//#include <urdf_parser/urdf_parser.h>
+
+// enum ControlType
+// {
+//     CONTROL_POSITION,
+//     CONTROL_VELOCITY,
+//     CONTROL_EFFORT
+// };
+
+template <class T1, class T2>
+struct RemapRule
+{
+    RemapRule(std::function<void(T1 *, T2 *)> lambda) : lambda(lambda){};
+    std::function<void(T1 *, T2 *)> lambda;
+    void apply(T1 *in_ptr, T2 *out_ptr) { lambda(in_ptr, out_ptr); };
+protected:
+    RemapRule(){};
+};
+
+template <class T1, class T2>
+struct RangeRemapRule : RemapRule<T1,T2>
+{
+    bool invert = false;
+    std::pair<T1, T1> from_range;
+    std::pair<T2, T2> to_range;
+    std::vector<size_t> from_idx;
+    std::vector<size_t> to_idx;
+
+    RangeRemapRule(
+        const std::pair<T1, T1>& from_range,
+        const std::pair<T2, T2>& to_range,
+        const std::vector<size_t>& from_idx,
+        const std::vector<size_t>& to_idx,
+        bool invert) : from_range(from_range), to_range(to_range), from_idx(from_idx), to_idx(to_idx), invert(invert)
+    {
+        this->lambda = [&](T1 *in_ptr, T2 *out_ptr)
+        {
+            for (size_t i = 0; i < from_idx.size(); i++)
+            {
+                size_t from = from_idx[i];
+                size_t to = to_idx[i];
+                if (invert)
+                    out_ptr[to] = to_range.second + (in_ptr[from] - from_range.first) * (to_range.first - to_range.second) / (from_range.second - from_range.first);
+                else
+                    out_ptr[to] = to_range.first + (in_ptr[from] - from_range.first) * (to_range.second - to_range.first) / (from_range.second - from_range.first);
+            }
+        };
+    };
+};
+
+// template <class T1, class T2>
+// struct JointRemapRule : RangeRemapRule<T1, T2>
+// {
+//     std::string joint = "";
+//     ControlType type = CONTROL_EFFORT;
+
+//     JointRemapRule(
+//         std::string &joint,
+//         std::pair<T1, T1> &from_range,
+//         std::pair<T2, T2> &to_range,
+//         ControlType type,
+//         bool invert) : joint(joint), type(type), RemapRule<T1, T2>(from_range, to_range, invert){};
+// };
+
+/** Transformations copy data from any number of buffers to a buffer in specified order.
+ *  Should be used whenever data is being copied
+ *  Remaps can be
+ *
+ *  Example:
+ *
+ *  `
+ *  - transform_1:
+ *      direction: ToController # It should always be clear which direction data is being transformed
+ *      to: to_buffer # Buffer we're copying data to
+ *      from:
+ *          - buffer_1: # Buffer we're copying data from
+ *              from_idx: [0,1,2]
+ *              to_idx: [0,1,2]
+ *          - buffer_2:
+ *              from_idx: [4,2,1,1] # Allow for copying elements in different order as well as repeats
+ *              to_idx: [6,5,4,3]
+ *              remap: actuator_1
+ *  `
+ *
+ */
+
+template <class T1, class T2>
+struct TransformRule
+{
+    T1 *from_buffer;
+    T2 *to_buffer;
+    std::vector<RemapRule<T1, T2>> rules;
+    TransformRule(T1 *from_buffer, T2 *to_buffer, const std::vector<RemapRule<T1, T2>> &rules) : from_buffer(from_buffer), to_buffer(to_buffer), rules(rules){};
+    TransformRule(T1 *from_buffer, T2 *to_buffer, const RemapRule<T1, T2> &rule) : from_buffer(from_buffer), to_buffer(to_buffer)
+    {
+        rules.emplace_back(rule);
+    };
+
+    void apply()
+    {
+        for (auto &r : rules)
+        {
+            r.apply(from_buffer, to_buffer);
+        }
+    }
+};
